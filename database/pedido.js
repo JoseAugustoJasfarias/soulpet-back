38cc2a03c466ff3c5fa836cfe96de148a4ba7fd6
VicScapini <victoria.scapini@gmail.com>
2023-04-27 17:20:26 -0300

    model-Pedido - BE18

---
+++ database/pedido.js
@@ -0,0 +1,27 @@
+
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const { v4: uuidv4 } = require('uuid');
+const Cliente = require("./cliente");
+const Produto = require("./produto");
+
+const Cliente = require("./cliente");
+const Produto = require("./produto");
+const Pedido = connection.define("pedido", {
+  codigo: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false
+  },
+  quantidade: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+// Associação 1:N (One-to-Many)
+Cliente.hasMany(Pedido);
+Pedido.belongsTo(Cliente);
+Produto.hasMany(Pedido);
+Pedido.belongsTo(Produto);
+module.exports = Pedido;

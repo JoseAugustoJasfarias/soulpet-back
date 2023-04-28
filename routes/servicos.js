31f97577dac625657ba228f47fe0575057de5beb
LuhD1 <damasceno.lucas.o@gmail.com>
2023-04-28 13:32:02 -0300

    Task#15 Lucas Damasceno

--- routes/servicos.js
+++ routes/servicos.js
@@ -6,7 +6,33 @@ const router = Router();
 router.get("/servicos", async (req, res) => {
   const listarServicos = await Servico.findAll();
   res.json(listarServicos);
-})
+});
+
+router.delete("/servico/:id", async (req, res) => {
+  const servico = await Servico.findByPk(req.params.id);
+
+  try {
+    if (servico) {
+      await servico.destroy();
+      res.json({ message: "O serviço foi removido.", servico });
+    } else {
+      res.status(404).json({ message: "O serviço não foi encontrado" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+router.delete("/servicos/deleteAll", async (req, res) => {
+  try {
+    await Servico.destroy({ where: {} });
+    res.json({ message: "Todos os serviços foram removidos." });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
 
 router.post("/servico", async (req, res) => {
   const { nome, preco } = req.body;

9deb092d387c4c39a925d684d5b160de12cb5917
Velsch21 <velsch.social@gmail.com>
2023-05-02 16:35:49 -0300

    BE-15

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -1,5 +1,7 @@
 const Agendamento = require("../database/agendamento");
 const { Router } = require("express");
+const Servico = require("../database/servico");
+const Pet = require("../database/pet");
 
 // Criar o grupo de rotas 
 const router = Router();
@@ -9,6 +11,28 @@ const router = Router();
 router.get("/agendamentos", async (req, res) => {
     const agendamentos = await Agendamento.findAll();
     res.json(agendamentos);
+});
+
+// POST
+router.post("/agendamentos", async (req, res) => {
+    const {servicoId, realizado, dataAgendada, petId } = req.body;
+    try {
+    const pet = await Pet.findByPk(petId);
+    const servico = await Servico.findByPk(servicoId);
+    if (pet) {
+        if (servico) {
+            const agendamento = await Agendamento.create({ servicoId, realizado, dataAgendada, petId });
+            res.status(201).json(agendamento);
+        } else {
+            res.status(404).json({ message: "Serviço não encontrado." });
+        }
+    } else {
+        res.status(404).json({ message: "Pet não encontrado." });
+    } 
+    } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+    }
 });
 
 module.exports = router;

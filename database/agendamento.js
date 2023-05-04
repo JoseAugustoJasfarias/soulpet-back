1f566bfe934923f9e6ef9edc4084fb9227f2a4c1
VicScapini <victoria.scapini@gmail.com>
2023-05-04 16:08:12 -0300

    Ajuste id Pedido

--- database/agendamento.js
+++ database/agendamento.js
@@ -1,13 +1,18 @@
 const { DataTypes } = require("sequelize");
 const { connection } = require("./database");
 
-    const Agendamento = connection.define("agendamento", {
-    dataAgendada: {
-        type: DataTypes.DATEONLY,
-    },
-    realizado: {
-        type: DataTypes.BOOLEAN
-    }
-    });
+const Agendamento = connection.define("agendamento", {
+  dataAgendada: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  realizada: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+});
 
-module.exports = Agendamento
+
+
+module.exports = Agendamento;
